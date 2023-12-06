081bf37f43d403e34b3e2c17a354df34c8cbf0e9
afonsosgsramalho <afonsosgsramalho@gmail.com>
2023-12-06 00:34:04 +0000

    day 3 changes

--- day_4/day4.py
+++ day_4/day4.py
@@ -1,25 +1,21 @@
 from collections import defaultdict
 
-lines = open('day4.txt').read().strip().split('\n')
+lines = open('day41.txt').read().strip().split('\n')
 
-number_cards = len(lines)
 games = 0
-cards = 0
 cards_dict = defaultdict(int)
 
 for card, line in enumerate(lines):
     cards_dict[card] += 1
-    copies = 0
     points = 0
     iter = 0
     w_numbers = line.split(':')[1].split('|')[0].split()
     m_numbers = line.split(':')[1].split('|')[1].split()
-    val = len(set(w_numbers) & set(m_numbers)) #one way to do it
+    val = len(set(w_numbers) & set(m_numbers)) # one way to do it
 
-    for i in w_numbers:
+    for i in w_numbers: # other way to do it
         if i in m_numbers:
             iter += 1
-            cards += 1
             if iter == 1:
                 points = 1
             else:
@@ -29,6 +25,8 @@ for card, line in enumerate(lines):
         cards_dict[card + 1 + j] += cards_dict[card]
 
     games += points
+    print('='*80)
 
+print(cards_dict)
 print(games)
 print(sum(cards_dict.values()))
